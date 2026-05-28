b34715b1c97d2975f1490893b8a411b9a7528aa1
amber-workspace <chiajung630102@gmail.com>
2026-05-28 11:14:30 +0800

    fix line sdk

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const app = express();
 
 // LINE
 const config = {
-  channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
-const client = new line.Client(config);
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+});
 
 // Google Credentials
 const creds = {
